dc8d4e9427549a56722c8f263f2a0638e5f6d96d
Zeke Hart <heirloch@gmail.com>
2020-01-09 17:03:56 -0500

    initial set up and installation of vue router and vue mq

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMq from 'vue-mq'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+Vue.use(VueMq, {
+  breakpoints: {
+    sm: 450,
+    md: 1250,
+    lg: Infinity
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
